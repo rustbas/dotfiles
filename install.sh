a275cd41651c9898d21c1f48913747948e2d3568
rustbas <hrustbas@gmail.com>
2023-01-24 13:47:16 +0000

    Some tools

--- install.sh
+++ install.sh
@@ -42,5 +42,9 @@ ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 mkdir -p "$XDG_DATA_HOME"
 cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 
+#########
+# Dunst #
+#########
 
-
+mkdir -p "$XDG_CONFIG_HOME/dunst"
+ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
